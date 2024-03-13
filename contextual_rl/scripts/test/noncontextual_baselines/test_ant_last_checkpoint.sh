0af2046fee4243ef8db37ba0809dfc328e8199f9
CheongWoong <cw.kang@kaist.ac.kr>
2024-03-13 11:51:22 +0900

    update

--- contextual_rl/scripts/test/noncontextual_baselines/test_ant_last_checkpoint.sh
+++ contextual_rl/scripts/test/noncontextual_baselines/test_ant_last_checkpoint.sh
@@ -1,18 +1,20 @@
-train_seed=$1
 test_seed=100
 
-for env_id in ContextualAntTestEasy1 ContextualAntTestEasy2 ContextualAntTestEasy3 \
-    ContextualAntTestEasy4 ContextualAntTestEasy5 ContextualAntTestEasy6 \
-    ContextualAntTestEasy7 ContextualAntTestEasy8 ContextualAntTestEasy9 \
-    ContextualAntTestHard1 ContextualAntTestHard2 ContextualAntTestHard3 ContextualAntTestHard4
+for train_seed in 0 1 2 3 4
 do
-    for run_name in Ant ContextualAntTrain $env_id
+    for env_id in ContextualAntTestEasy1 ContextualAntTestEasy2 ContextualAntTestEasy3 \
+        ContextualAntTestEasy4 ContextualAntTestEasy5 ContextualAntTestEasy6 \
+        ContextualAntTestEasy7 ContextualAntTestEasy8 ContextualAntTestEasy9 \
+        ContextualAntTestHard1 ContextualAntTestHard2 ContextualAntTestHard3 ContextualAntTestHard4
     do
-        run_dir="runs/training/seed_"$train_seed"/"$run_name
-        checkpoint_path=$run_dir"/checkpoints/99942400.pth"
-        python -m src.test_ppo \
-            --checkpoint_path $checkpoint_path \
-            --env_id $env_id \
-            --seed $test_seed
+        for run_name in Ant ContextualAntTrain $env_id
+        do
+            run_dir="runs/training/seed_"$train_seed"/"$run_name
+            checkpoint_path=$run_dir"/checkpoints/99942400.pth"
+            python -m src.test_ppo \
+                --checkpoint_path $checkpoint_path \
+                --env_id $env_id \
+                --seed $test_seed
+        done
     done
 done
--- contextual_rl/scripts/test/noncontextual_baselines/test_anymal_last_checkpoint.sh
+++ contextual_rl/scripts/test/noncontextual_baselines/test_anymal_last_checkpoint.sh
@@ -1,18 +1,20 @@
-train_seed=$1
 test_seed=100
 
-for env_id in ContextualAnymalTestEasy1 ContextualAnymalTestEasy2 ContextualAnymalTestEasy3 \
-    ContextualAnymalTestEasy4 ContextualAnymalTestEasy5 ContextualAnymalTestEasy6 \
-    ContextualAnymalTestEasy7 ContextualAnymalTestEasy8 ContextualAnymalTestEasy9 \
-    ContextualAnymalTestHard1 ContextualAnymalTestHard2 ContextualAnymalTestHard3 ContextualAnymalTestHard4
+for train_seed in 0 1 2 3 4
 do
-    for run_name in Anymal ContextualAnymalTrain $env_id
+    for env_id in ContextualAnymalTestEasy1 ContextualAnymalTestEasy2 ContextualAnymalTestEasy3 \
+        ContextualAnymalTestEasy4 ContextualAnymalTestEasy5 ContextualAnymalTestEasy6 \
+        ContextualAnymalTestEasy7 ContextualAnymalTestEasy8 ContextualAnymalTestEasy9 \
+        ContextualAnymalTestHard1 ContextualAnymalTestHard2 ContextualAnymalTestHard3 ContextualAnymalTestHard4
     do
-        run_dir="runs/training/seed_"$train_seed"/"$run_name
-        checkpoint_path=$run_dir"/checkpoints/99942400.pth"
-        python -m src.test_ppo \
-            --checkpoint_path $checkpoint_path \
-            --env_id $env_id \
-            --seed $test_seed
+        for run_name in Anymal ContextualAnymalTrain $env_id
+        do
+            run_dir="runs/training/seed_"$train_seed"/"$run_name
+            checkpoint_path=$run_dir"/checkpoints/99942400.pth"
+            python -m src.test_ppo \
+                --checkpoint_path $checkpoint_path \
+                --env_id $env_id \
+                --seed $test_seed
+        done
     done
 done
